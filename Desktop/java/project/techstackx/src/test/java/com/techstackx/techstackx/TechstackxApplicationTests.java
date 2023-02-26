e1658a8d6b55ba64b25f1458116378ef529a0fde
chandan kumar yadav <cy93011@gmail.com>
2023-02-26 20:03:30 +0530

    test

---
+++ Desktop/java/project/techstackx/src/test/java/com/techstackx/techstackx/TechstackxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstackx.techstackx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstackxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
